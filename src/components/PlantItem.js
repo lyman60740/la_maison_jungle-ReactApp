5e0e29205ebbc3d8898dcd3f978f88ac17c3badc
lyman60740 <93662090+lyman60740@users.noreply.github.com>
2022-06-27 17:20:39 +0200

    Add files via upload

---
+++ src/components/PlantItem.js
@@ -0,0 +1,23 @@
+import CareScale from './CareScale'
+import '../styles/PlantItem.css'
+
+function handleClick(e) {
+    console.log('✨ Ceci est mon event :', e)
+}
+function PlantItem({ id, cover, name, water, light }) {
+	return (
+		<li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
+			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
+			{name}
+			<div>
+				<CareScale careType='water' scaleValue={water} />
+				<CareScale careType='light' scaleValue={light} />
+			</div>
+		</li>
+	)
+    
+}
+
+
+
+export default PlantItem
---
+++ src/components/Recommendation.js
@@ -0,0 +1,12 @@
+function Recommendation (){
+    const currentMonth = new Date().getMonth()
+    const isSpring = currentMonth >= 2 && currentMonth <= 5
+    
+    if (!isSpring){
+        return <div> Ce n'est pas le moment de rempoter</div>
+    }
+    return <div>C'est le printemps, rempotez !</div>
+    
+}
+
+export default Recommendation
